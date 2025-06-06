854d7fa0265e4e19d2a5c761720acb8b8ee423d1
Addie Rohrbach <addier@piccobev.com>
2025-06-06 18:49:28 -0500

    add shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "shadcn-nuxt"],
+  css: ["~/assets/css/main.css"],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
 });
