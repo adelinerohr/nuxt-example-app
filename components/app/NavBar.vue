2cd357964b39f52938cc05767ce2fd301473593d
Addie Rohrbach <addier@piccobev.com>
2025-06-07 10:23:06 -0500

    add theme toggle

--- components/app/NavBar.vue
+++ components/app/NavBar.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="border-b">
     <div class="container py-4 flex items-center justify-between">
-      <NuxtLink to="/" class="text-xl font-medium">nuxt-example-app</NuxtLink>
+      <NuxtLink to="/" class="text-xl font-medium">
+        nuxt-example-app
+      </NuxtLink>
       <div class="flex items-center gap-4">
+        <AppThemeToggle />
         <Button> Sign in </Button>
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
   <div class="container py-44">
     <div class="flex flex-col items-center gap-8">
       <div class="space-y-4 text-center">
-        <h1 class="text-7xl font-medium">Example Nuxt Application</h1>
+        <h1 class="text-7xl font-medium">
+          Example Nuxt Application
+        </h1>
         <p class="text-xl text-muted-foreground">
           Nuxt UI Pro is a collection of premium Vue components built on top of
           Nuxt UI to create beautiful & responsive Nuxt applications in minutes.
