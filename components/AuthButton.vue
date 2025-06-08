5d133780cfb101a8810f1b57e578270c845e25e4
Addie Rohrbach <addier@piccobev.com>
2025-06-07 23:45:04 -0500

    add log out functionality

--- components/AuthButton.vue
+++ components/AuthButton.vue
@@ -3,7 +3,53 @@ const authStore = useAuthStore();
 </script>
 
 <template>
+  <DropdownMenu v-if="!authStore.loading && authStore.user">
+    <DropdownMenuTrigger class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground flex items-center px-2.5 py-1.5 gap-2 rounded-lg">
+      <Avatar class="size-8 rounded-full">
+        <AvatarImage
+          v-if="authStore.user.image"
+          :src="authStore.user.image"
+          :alt="authStore.user.name"
+        />
+        <AvatarFallback v-else class="rounded-full">
+          {{ authStore.user.name.charAt(0) }}
+        </AvatarFallback>
+      </Avatar>
+      <div class="grid flex-1 text-left text-sm leading-tight">
+        <span class="truncate font-semibold">{{ authStore.user.name }}</span>
+        <span class="truncate text-xs">{{ authStore.user.email }}</span>
+      </div>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuLabel>
+        <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+          <Avatar class="h-8 w-8 rounded-full">
+            <AvatarImage
+              v-if="authStore.user.image"
+              :src="authStore.user.image"
+              :alt="authStore.user.name"
+            />
+            <AvatarFallback v-else class="rounded-full">
+              CN
+            </AvatarFallback>
+          </Avatar>
+          <div class="grid flex-1 text-left text-sm leading-tight">
+            <span class="truncate font-semibold">{{ authStore.user.name }}</span>
+            <span class="truncate text-xs">{{ authStore.user.email }}</span>
+          </div>
+        </div>
+      </DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <NuxtLink to="/sign-out">
+        <DropdownMenuItem>
+          <Icon name="tabler:logout" />
+          Sign out
+        </DropdownMenuItem>
+      </NuxtLink>
+    </DropdownMenuContent>
+  </DropdownMenu>
   <Button
+    v-else
     :disabled="authStore.loading"
     :loading="authStore.loading"
     @click="authStore.signIn"
---
+++ pages/sign-out.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="container mt-4 min-h-96 mx-auto flex items-center justify-center flex-col gap-6 bg-secondary rounded-2xl">
+    <div
+      class="animate-spin inline-block size-12 border-3 border-current border-t-transparent text-black rounded-full dark:text-white"
+      role="status"
+      aria-label="loading"
+    >
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+</template>
